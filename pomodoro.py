46d8d25aff324b6942638ffe82a81693456873ea
Felix-Raj <felix.raj@riverbed.com>
2019-03-13 13:45:50 +0530

    add some messages

--- pomodoro.py
+++ pomodoro.py
@@ -5,17 +5,21 @@ class Pomodoro:
         self.short_break = short_break
         self.long_break = long_break
         self.cycle = cycle
-        self.start_state = PomodoroState(pomodoro_time)
+        self.start_state = PomodoroState(pomodoro_time, message='In Pomodoro')
         self.current_state = self.start_state
         self.create_states()
 
     def create_states(self):
         current_state = self.start_state
         for _ in range(self.cycle):
-            current_state.next_state = PomodoroState(self.short_break)
-            current_state.next_state.next_state = PomodoroState(self.pomodoro_time)
+            current_state.next_state = PomodoroState(self.short_break,
+                                                     message='Take Short Break')
+            current_state.next_state.next_state = PomodoroState(
+                self.pomodoro_time, message='In Pomodoro')
             current_state = current_state.next_state.next_state
-        current_state.next_state = PomodoroState(self.long_break, self.start_state)
+        current_state.next_state = PomodoroState(self.long_break,
+                                                 self.start_state,
+                                                 'Take Long Break')
 
     def next_state(self):
         _next_state = self.current_state.next_state
@@ -24,9 +28,12 @@ class Pomodoro:
 
 
 class PomodoroState:
-    def __init__(self, time_out, next_state=None):
+    def __init__(self, time_out, next_state=None, message=None):
         self.time_out = time_out
         self.next_state = next_state
+        self._message = message
 
     def message(self):
+        if self._message:
+            return self._message
         return 'Next reminder in {} minutes'.format(self.time_out/60)
